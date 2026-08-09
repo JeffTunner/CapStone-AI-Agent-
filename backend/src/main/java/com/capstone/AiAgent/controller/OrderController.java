d686580e836452ec115ded082f2fddeb6ca85abf
JeffTunner <abhinavkaushik05@gmail.com>
2026-08-09 23:28:06 +0530

    feat: order controller

--- backend/src/main/java/com/capstone/AiAgent/controller/OrderController.java
+++ backend/src/main/java/com/capstone/AiAgent/controller/OrderController.java
@@ -1,21 +1,17 @@
 package com.capstone.AiAgent.controller;
 
 import com.capstone.AiAgent.dto.OrderItemRequestDto;
+import com.capstone.AiAgent.dto.OrderRequestDto;
 import com.capstone.AiAgent.dto.OrderResponseDto;
 import com.capstone.AiAgent.mapper.OrderMapper;
 import com.capstone.AiAgent.model.Order;
 import com.capstone.AiAgent.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
-
-record PlaceOrder(UUID customerId, List<OrderItemRequestDto> items) {}
 
 @RestController
 @RequestMapping("/api/orders")
@@ -25,8 +21,14 @@ public class OrderController {
     OrderService orderService;
 
     @PostMapping
-    public ResponseEntity<OrderResponseDto> order(@RequestBody PlaceOrder orderRequest) {
-        OrderResponseDto order = orderService.placeOrder(orderRequest.customerId(), orderRequest.items());
+    public ResponseEntity<OrderResponseDto> order(@RequestBody OrderRequestDto orderRequest) {
+        OrderResponseDto order = orderService.placeOrder(orderRequest);
         return ResponseEntity.ok().body(order);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderResponseDto>> getAll() {
+        List<OrderResponseDto> orders = orderService.getOrders();
+        return ResponseEntity.ok().body(orders);
     }
 }
